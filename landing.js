29dd3858399ea82dac3e179b3404c6d1b2173edd
saiteja2873 <125501932+saiteja2873@users.noreply.github.com>
2024-06-22 09:40:20 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,10 @@
+function hello(){
+    document.getElementById("a111").innerHTML = "Close "
+    document.getElementById("a4").innerHTML = "&#10006"
+    }
+
+    // document.addEventListener("DOMContentLoaded", function() { 
+    //     // Add the fade-in class to the element you want to fade in 
+    //     document.getElementById("a111").classList.add("fade-in"); 
+    // }); 
+
